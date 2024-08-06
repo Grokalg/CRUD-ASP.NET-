a38b3d73bc3a3d748294e753d93c37aefae3452e
Grokalg <98714600+Grokalg@users.noreply.github.com>
2024-08-06 23:19:38 +0300

    ProfileController
    Добавлена возможность редактирования профиля пользователя

--- CRUD(ASP.NET)/Controllers/ProfileController.cs
+++ CRUD(ASP.NET)/Controllers/ProfileController.cs
@@ -9,8 +9,8 @@ namespace CRUD_ASP.NET_.Controllers
 		{
 			using (var db = new Context())
 			{
-				var user1 = db.Users.Find(user.Id);
-				return View(user1);
+				var db_user = db.Users.Find(user.Id);
+				return View(db_user);
 			}	
 		}
 
@@ -18,9 +18,26 @@ namespace CRUD_ASP.NET_.Controllers
 		{
 			using (var db = new Context())
 			{
-				var user1 = db.Users.Find(user.Id);
-				return View(user1);
+				var db_user = db.Users.Find(user.Id);
+				return View(db_user);
 			}
+		}
+
+		public IActionResult Check(User user)
+		{
+			if (ModelState.IsValid)
+			{
+				using (var db = new Context())
+				{
+					var db_user = db.Users.Find(user.Id);
+					db_user.Name = user.Name;
+					db_user.Email = user.Email;
+					db_user.Password = user.Password;
+					db.SaveChanges();
+					return View("Index", user);
+				}
+			}
+			return View("Edit");
 		}
 	}
 }
